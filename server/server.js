fd7fb79b65fcc1ff97b63cab0e383fff630bd5d9
Daniel Cabrera <142362683+DCabrera290@users.noreply.github.com>
2024-04-04 00:14:53 +0000

    4/3/24 5:15pm

--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 const { generateKey } = require('crypto');
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors')
 const app = express();
+app.use(cors());
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+    // res.send('Hello, World!');
 });
 
-app.listen(port, () => {
+app.get('/csv-data', (req, res) => {
+    const csvPath = path.join(__dirname, '..', 'data', 'zonal_sea_averages.csv');
+    fs.readFile(csvPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        res.type('text/plain');
+        res.send(data);
+    });
+})
+
+app.listen(port, (req, res) => {
     console.log(`Server is listening on port ${port}`);
 })
 
@@ -15,3 +32,4 @@ app.listen(port, () => {
 app.get('/ifrair729', (req, res) => {
     res.send('Hello, Isaac! I\'m better than you at typing! LOL!');
 });
+
